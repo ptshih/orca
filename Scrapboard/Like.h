3251e5620fac654c81da15dc788c65ae5e03bf5b
Peter Shih <ptshih@gmail.com>
2011-05-04 17:45:42 -0700

    adding like model

---
+++ Scrapboard/Like.h
@@ -0,0 +1,24 @@
+//
+//  Like.h
+//  Scrapboard
+//
+//  Created by Peter Shih on 5/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Snap;
+
+@interface Like : NSManagedObject {
+@private
+}
+@property (nonatomic, retain) NSString * id;
+@property (nonatomic, retain) NSString * userId;
+@property (nonatomic, retain) NSString * userName;
+@property (nonatomic, retain) NSString * userPictureUrl;
+@property (nonatomic, retain) NSDate * timestamp;
+@property (nonatomic, retain) Snap * snap;
+
+@end
---
+++ Scrapboard/Like.m
@@ -0,0 +1,22 @@
+//
+//  Like.m
+//  Scrapboard
+//
+//  Created by Peter Shih on 5/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Like.h"
+#import "Snap.h"
+
+
+@implementation Like
+@dynamic id;
+@dynamic userId;
+@dynamic userName;
+@dynamic userPictureUrl;
+@dynamic timestamp;
+@dynamic snap;
+
+
+@end
