0f9a3efc6e8984e3e37becacfc55da667a5f3323
Peter Shih <ptshih@gmail.com>
2011-05-14 01:09:52 -0700

    bubbles staging server

--- Scrapboard/NetworkConstants.h
+++ Scrapboard/NetworkConstants.h
@@ -27,7 +27,7 @@
   #ifdef USE_LOCALHOST
     #define API_BASE_URL [NSString stringWithFormat:@"http://localhost:3000/%@", API_VERSION]
   #else
-    #define API_BASE_URL [NSString stringWithFormat:@"http://ohsnaplabs.com/%@", API_VERSION]
+    #define API_BASE_URL [NSString stringWithFormat:@"http://bubbles.ohsnaplabs.com:3000/%@", API_VERSION]
   #endif
 #else
   #define API_BASE_URL [NSString stringWithFormat:@"http://ohsnaplabs.com/%@", API_VERSION]
