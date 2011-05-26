f0dc2cd3f7f60c2b7d624b726307e65df12cca5a
Peter Shih <ptshih@gmail.com>
2011-05-26 14:58:21 -0700

    uiimage nscoding for archiving image cache

---
+++ PhotoFeed/UIImage+NSCoding.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+NSCoding.h
+//  PhotoFeed
+//
+//  Created by Peter Shih on 5/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIImage (UIImage_NSCoding)
+
+@end
---
+++ PhotoFeed/UIImage+NSCoding.m
@@ -0,0 +1,14 @@
+//
+//  UIImage+NSCoding.m
+//  PhotoFeed
+//
+//  Created by Peter Shih on 5/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIImage+NSCoding.h"
+
+
+@implementation UIImage (UIImage_NSCoding)
+
+@end
