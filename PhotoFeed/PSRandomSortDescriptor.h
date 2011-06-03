cbbc99bb107e6fa6aa409507af0c975713eb6d3f
Peter Shih <ptshih@gmail.com>
2011-06-02 18:52:45 -0700

    assets

---
+++ PhotoFeed/PSRandomSortDescriptor.h
@@ -0,0 +1,16 @@
+//
+//  PSRandomSortDescriptor.h
+//  PhotoFeed
+//
+//  Created by Peter Shih on 6/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PSRandomSortDescriptor : NSObject {
+    
+}
+
+@end
---
+++ PhotoFeed/PSRandomSortDescriptor.m
@@ -0,0 +1,14 @@
+//
+//  PSRandomSortDescriptor.m
+//  PhotoFeed
+//
+//  Created by Peter Shih on 6/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PSRandomSortDescriptor.h"
+
+
+@implementation PSRandomSortDescriptor
+
+@end
